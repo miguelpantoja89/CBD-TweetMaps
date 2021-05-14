ce3151c944b842ec937c3030141258ca7f236e14
Miguel Angel <mgbastechmology@gmail.com>
2021-05-14 12:59:59 +0200

    PreFinal Version

--- TweetMaps/Tweets/models.py
+++ TweetMaps/Tweets/models.py
@@ -3,10 +3,13 @@ from django.db import models
 
 class Tweet(models.Model):
     text = models.CharField(max_length=150,default='Titulo')
-    created_at = models.DateTimeField(auto_now=True)
+    posted = models.DateTimeField()
     titulo = models.TextField(default='default message')
     latitud = models.FloatField()
     longitud = models.FloatField()
     user = models.CharField(max_length=150,default='usuario')
     url = models.URLField(max_length=200,default='url/gsj')
     location = models.CharField(max_length=150,default='paisciudad')
+    language = models.CharField(max_length=150,default='idioma')
+    num_retweets = models.IntegerField(default=0)
+    source = models.CharField(max_length=150,default='Android')
--- TweetMaps/Tweets/views.py
+++ TweetMaps/Tweets/views.py
@@ -3,34 +3,83 @@ from django.views.generic.base import TemplateView
 from .models import Tweet
 import tweepy
 import datetime
+from django.db.models import Max
 from django.conf import settings
-# Create your views here.
-def search_hashtag(request):
-    hashtag = request.GET.get('search')
-    Tweet.objects.all().delete()
-    auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
-    auth.set_access_token(settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth,wait_on_rate_limit=True)
-    tweets_list = tweepy.Cursor(api.search, q=hashtag+" -filter:retweets",tweet_mode='extended').items()
-    for tweet in tweets_list:
-        text = tweet._json["full_text"]
-        created_at =  tweet.created_at.strftime('%m/%d/%Y %H:%M')
-        if(tweet.place is None):
-            coordinates = [[['-5.97317','37.38283']]]
-        else:
-            coordinates = tweet.place.bounding_box.coordinates
-            Tweet.objects.create(text="hola", created_at=created_at, titulo=text, latitud=coordinates[0][0][1], longitud=coordinates[0][0][0])
-    return redirect("/tweets/api/map")
 
+class AllTweetsView(TemplateView):
+    template_name="tweets.html"
+    def get(self, request):
+        tweets = Tweet.objects.all()
+        half = int(len(tweets)/2)
+        tw1 = tweets[0:half]
+        tw2 = tweets[half:(len(tweets))]
+        
+        return render(request, self.template_name, {'tweets1':tw1, 'tweets2':tw2})
 class MarkersMapView(TemplateView):
     template_name = "map.html"
     def get(self, request):
         marker = Tweet.objects.all()
-        marker2 = Tweet.objects.all()[0:11]
-        return render(request, self.template_name, {'data':'hola','patata':marker, 'patata2':marker2})
+        marker2 = Tweet.objects.all()[0:4]
+        tweet_max_rt = Tweet.objects.aggregate(num_retweets=Max('num_retweets'))['num_retweets']
+        tweet_RT = Tweet.objects.filter(num_retweets=tweet_max_rt)
+        idiomas = Tweet.objects.values('language').distinct()
+        lang = []
+        tweets_byLang = []
+        for i in idiomas:
+            lang.append(i['language'])
+            tweets_byLang.append(Tweet.objects.filter(language=i['language']).count())
+        totalL= zip(lang,tweets_byLang)
+        return render(request, self.template_name, {'data':'hola','patata':marker, 'patata2':marker2, 'maxRT':tweet_RT, 'idiomas':totalL})
 
 class StatisticsView(TemplateView):
     template_name="statistics.html"
+    def get(self,request):
+        source_type_app = Tweet.objects.filter(source='App').count()
+        source_type_web = Tweet.objects.filter(source='Web').count()
+        countries = Tweet.objects.values('location').distinct()
+        tweets = Tweet.objects.all()
+        times_m = []
+        times_t = []
+        fecha_semana= []
+        cantidad_tweet_dia = []
+        mid_day = datetime.time(12,0,0)
+        for t in tweets:
+            if(t.posted.time() < mid_day):
+                times_m.append(t.posted.time())
+            if(t.posted.time() > mid_day):
+                times_t.append(t.posted.time())
+            if(t.posted.date() > datetime.datetime.now().date() - datetime.timedelta(days=7)):
+                fecha_semana.append(t.posted.date().strftime('%A'))
+        l,m,x,j,v,s,d = 0,0,0,0,0,0,0
+        for f in fecha_semana:
+            if(f =='Monday'):
+                l+=1
+            if(f =='Tuesday'):
+                m+=1
+            if(f =='Wednesday'):
+                x+=1
+            if(f =='Thursday'):
+                j+=1
+            if(f =='Friday'):
+                v+=1
+            if(f =='Saturday'):
+                s+=1
+            if(f =='Sunday'):
+                d+=1
+        cantidad_tweet_dia.append(l)
+        cantidad_tweet_dia.append(m)  
+        cantidad_tweet_dia.append(x)  
+        cantidad_tweet_dia.append(j)  
+        cantidad_tweet_dia.append(v)  
+        cantidad_tweet_dia.append(s)  
+        cantidad_tweet_dia.append(d)  
+
+        paises = []
+        tweets_byCountry = []
+        for i in countries:
+            paises.append(i['location'])
+            tweets_byCountry.append(Tweet.objects.filter(location=i['location']).count())
+        return render(request, self.template_name, {'web':source_type_web, 'app':source_type_app, 'paises':paises, 'tweetsPorPais':tweets_byCountry, 'horasM':len(times_m), 'horasT':len(times_t), 'semanaLast': cantidad_tweet_dia})
     
 
 class StoreTweetsGoats(TemplateView):
@@ -42,33 +91,29 @@ class StoreTweetsGoats(TemplateView):
         api = tweepy.API(auth,wait_on_rate_limit=True)
         today = datetime.date.today()
         yesterday= today - datetime.timedelta(days=1)
-        #'Bengali': 'bn'	'Hebrew': 'iw'	'Norwegian': 'no'	'Swedish': 'sv'
-# 'Bosnian': 'bs'	'Hindi': 'hi'	'Oriya': 'or'	'Tagalog': 'tl'
-# 'Bulgarian': 'bg'	'Latinized Hindi': 'hi-Latn'	'Panjabi': 'pa'	'Tamil': 'ta'
-# 'Burmese': 'my'	'Hungarian': 'hu'	'Pashto': 'ps'	'Telugu': 'te'
-# 'Croatian': 'hr'	'Icelandic': 'is'	'Persian': 'fa'	'Thai': 'th'
-# 'Catalan': 'ca'	'Indonesian': 'in'	'Polish': 'pl'	'Tibetan': 'bo'
-# 'Czech': 'cs'	'Italian': 'it'	'Portuguese': 'pt'	'Traditional Chinese':'zh-TW'
-# 'Danish': 'da'	'Japanese': 'ja'	'Romanian': 'ro'	'Turkish': 'tr'
-# 'Dutch': 'nl'	'Kannada': 'kn'	'Russian': 'ru'	'Ukrainian': 'uk'
-# 	'Khmer': 'km'	'Serbian': 'sr'	'Urdu': 'ur'
-# 'Estonian': 'et'	'Korean': 'ko'	'Simplified Chinese': 'zh-CN'	'Uyghur': 'ug'
-# 'Finnish': 'fi'	'Lao': 'lo'	'Sindhi': 'sd'	'Vietnamese': 'vi'
-# 'French': 'fr'	'Latvian': 'lv'	'Sinhala': 'si'	'Welsh': 'cy'
-# 'Georgian': 'ka'	'Lithuanian': 'lt'
         bcp47_dictionary = { 'am':'Amharic', 'de':'German', 'ml':'Malayalam', 'sk':'Slovak','ar' : 
 'Arabic', 'el':'Greek','dv': 'Maldivian', 'sl':'Slovenian',
-'hy': 'Armenian', 'gu' : 'Gujarati', 'mr': 	'Marathi', 'ckb': 'Kurdish','eu' : 
-'Basque', 'ht':'Creole',  'ne': 'Nepali','es':	'Spanish', 'ja':'Japanese','en':'English','tl':'Tagalog','und':'Undefined',
+'hy': 'Armenian', 'gu' : 'Gujarati', 'mr':     'Marathi', 'ckb': 'Kurdish','eu' : 
+'Basque', 'ht':'Creole',  'ne': 'Nepali','es':    'Spanish', 'ja':'Japanese','en':'English','tl':'Tagalog','und':'Undefined',
+'bn':'Bengali', 'iw':'Hebrew', 'no':'Norwegian', 'sv': 'Swedish', 'bs':'Bosnian', 'hi':'Hindi', 'or':'Oriya', 'tl':'Tagalog', 'bg':'Bulgarian', 
+'hi-Latn':'Latinized Hindi', 'pa':'Panjabi', 'ta':'Tamil', 'my':'Burmese', 'hu': 'Hungarian', 'ps': 'Pashto', 'te': 'Telugu', 'hr': 'Croatian',
+'is': 'Icelandic', 'fa': 'Persian', 'th': 'Thai', 'ca': 'Catalan', 'in': 'Indonesian', 'pl': 'Polish', 'bo': 'Tibetan', 'cs':'Czech', 'it': 'Italian',
+'pt': 'Portuguese', 'zh-TW': 'Traditional Chinese', 'da': 'Danish', 'ro': 'Romanian', 'tr': 'Turkish', 'nl': 'Dutch', 'kn': 'Kannada', 'ru': 'Russian', 
+'uk': 'Ukranian', 'km': 'Khmer', 'sr':'Serbian', 'ur':'Urdu', 'et': 'Estonian', 'ko': 'Koraean', 'zh-CN': 'Simplified Chinese', 'ug': 'Uyghur', 
+'fi': 'Finnish', 'lo':'Lao', 'sd': 'Sindhi', 'vi': 'Vietnamese', 'fr': 'French', 'lv': 'Latvian', 'si': 'Sinhala', 'cy': 'Welsh', 'ka': 'Georgian', 'lt': 'Lituanian'
         }
         tweets_list = tweepy.Cursor(api.search, q="#WeLoveGoats -filter:retweets",tweet_mode='extended').items()
         for tweet in tweets_list:
             text = tweet._json["full_text"]
-            created_at =  tweet.created_at.strftime('%m/%d/%Y %H:%M')
+            created_at =  tweet.created_at.strftime('%Y-%m-%d %H:%M')
             username = tweet.user.screen_name 
             # tweet.id
             # retweet_count
-            # bcp47_dictionary[tweet.lang]
+            idioma =  bcp47_dictionary[tweet.lang]
+            if('Web' in tweet.source):
+                source_type = 'Web'
+            else:
+                source_type = 'App'
             
             url = "https://twitter.com/"+username+"/status/"+str(tweet.id)
 
@@ -78,7 +123,8 @@ class StoreTweetsGoats(TemplateView):
             else:
                 country = tweet.place.country
                 coordinates = tweet.place.bounding_box.coordinates
-            Tweet.objects.create(text=text[0:12], user=username, location=country, url=url, created_at=created_at, titulo=text, latitud=coordinates[0][0][1], longitud=coordinates[0][0][0])
+            Tweet.objects.create(text=text[0:12], user=username, location=country, url=url, posted=created_at, titulo=text, latitud=coordinates[0][0][1], longitud=coordinates[0][0][0], language=idioma,num_retweets=tweet.retweet_count,
+            source=source_type)
        
 
     
