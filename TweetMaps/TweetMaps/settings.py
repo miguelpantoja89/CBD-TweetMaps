e5e350cd1934bbdeaa735e015bcc546638420a9e
Miguel Angel <mgbastechmology@gmail.com>
2021-05-22 02:04:06 +0200

    DockerHub

--- TweetMaps/TweetMaps/settings.py
+++ TweetMaps/TweetMaps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z68mk3(0g0@zr505t*q4*4tha(y7&vxu5g^m+%4u(n^hz)_2pc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','localhost']
 
 
 # Application definition
--- TweetMaps/Tweets/management/commands/setup.py
+++ TweetMaps/Tweets/management/commands/setup.py
@@ -1,10 +1,16 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
+import os
+from pathlib import Path
+
+
 
 class Command(BaseCommand):
     help = 'Sets up the system'
 
     def handle(self, *args, **kwargs):
+        if(os.path.isdir('./migrations')):
+            call_command("migrate")
         call_command("migrate")
         call_command("makemigrations", "Tweets")
         call_command("migrate","Tweets")
